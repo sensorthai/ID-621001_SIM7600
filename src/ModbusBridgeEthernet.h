111c750e4f4fdb3c8092434a39e846f7cd1b2617
Ponnapa Karanan <ponnapa@wirelesssolution.asia>
2021-07-19 12:15:13 +0700

    eModbus

---
+++ src/ModbusBridgeEthernet.h
@@ -0,0 +1,17 @@
+// =================================================================================================
+// eModbus: Copyright 2020 by Michael Harwerth, Bert Melis and the contributors to eModbus
+//               MIT license - see license.md for details
+// =================================================================================================
+#ifndef _MODBUS_BRIDGE_ETHERNET_H
+#define _MODBUS_BRIDGE_ETHERNET_H
+#include "options.h"
+#if HAS_ETHERNET == 1
+#include <Ethernet.h>
+#include <SPI.h>
+#include "ModbusServerTCPtemp.h"
+#include "ModbusBridgeTemp.h"
+
+using ModbusBridgeEthernet = ModbusBridge<ModbusServerTCP<EthernetServer, EthernetClient>>;
+#endif
+
+#endif
---
+++ src/ModbusServerWiFi.h
@@ -0,0 +1,13 @@
+// =================================================================================================
+// eModbus: Copyright 2020 by Michael Harwerth, Bert Melis and the contributors to eModbus
+//               MIT license - see license.md for details
+// =================================================================================================
+#ifndef _MODBUS_SERVER_WIFI_H
+#define _MODBUS_SERVER_WIFI_H
+#include "options.h"
+#include <WiFi.h>
+#include "ModbusServerTCPtemp.h"
+
+using ModbusServerWiFi = ModbusServerTCP<WiFiServer, WiFiClient>;
+
+#endif
